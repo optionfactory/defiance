8f9f2d2bfda171bb9d195f04b8f38fd298890e3d
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2020-01-22 15:44:13 +0100

    :fix: bot broken bump

--- src/main/java/net/optionfactory/defiance/DirectFieldAccessConfiguration.java
+++ src/main/java/net/optionfactory/defiance/DirectFieldAccessConfiguration.java
@@ -1,20 +1,20 @@
 package net.optionfactory.defiance;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 
 @Configuration
 public class DirectFieldAccessConfiguration extends DelegatingWebMvcConfiguration {
-
     @Override
-    protected ConfigurableWebBindingInitializer getConfigurableWebBindingInitializer() {
+    protected ConfigurableWebBindingInitializer getConfigurableWebBindingInitializer(FormattingConversionService mvcConversionService, Validator mvcValidator) {
         ConfigurableWebBindingInitializer initializer = new ConfigurableWebBindingInitializer();
         initializer.setDirectFieldAccess(true);
-        initializer.setConversionService(mvcConversionService());
-        initializer.setValidator(mvcValidator());
+        initializer.setConversionService(mvcConversionService);
+        initializer.setValidator(mvcValidator);
         initializer.setMessageCodesResolver(getMessageCodesResolver());
         return initializer;
-
     }
 }
